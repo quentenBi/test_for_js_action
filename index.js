3231e803cb8fa6176039560364a8151de752c19a
ChenleiBi <quenten.c.bi@pwc.com>
2019-09-24 18:24:43 +0800

    test actions-toolkit

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { Toolkit } = require('actions-toolkit');
+const tools = new Toolkit();
 
 try {
   const name = core.getInput('greet');
   console.log(`Hello ${name}`);
   core.setOutput('time', new Date().toTimeString());
+
+  console.log(JSON.stringify(tools.context), void 0, 2);
+  console.log('\n\n');
+  console.log(JSON.stringify(tools.github), void 0, 2);
 } catch (err) {
   core.setFailed(err.message);
 }
