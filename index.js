de25773b91bf18d63d0c39550abf62f110ffd237
ChenleiBi <quenten.c.bi@pwc.com>
2019-09-24 18:29:39 +0800

    update

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ try {
   console.log(`Hello ${name}`);
   core.setOutput('time', new Date().toTimeString());
 
-  console.log(JSON.stringify(tools.context), void 0, 2);
+  console.log('------------ tools.context info ---------------');
+  console.log(JSON.stringify(tools.context, void 0, 2));
   console.log('\n\n');
-  console.log(JSON.stringify(tools.github), void 0, 2);
+  console.log('------------ tools.github info ---------------');
+  console.log(JSON.stringify(tools.github, void 0, 2));
 } catch (err) {
   core.setFailed(err.message);
 }
